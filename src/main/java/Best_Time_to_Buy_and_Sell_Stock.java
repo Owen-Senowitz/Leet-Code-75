4ae599ac7af4d235b22a220e82c0909ef13df5be
Owen Senowitz <owen.senowitz@gmail.com>
2022-09-07 22:43:16 -0400

    Best_Time_to_Buy_and_Sell_Stock completed

---
+++ src/main/java/Best_Time_to_Buy_and_Sell_Stock.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class Best_Time_to_Buy_and_Sell_Stock {
+    public static int maxProfit(int[] prices) {
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minprice)
+                minprice = prices[i];
+            else if (prices[i] - minprice > maxprofit)
+                maxprofit = prices[i] - minprice;
+        }
+        return maxprofit;
+    }
+    public static void main(String[] args) {
+        int[] prices = {7,1,5,3,6,4};
+        System.out.println(maxProfit(prices));
+    }
+}
