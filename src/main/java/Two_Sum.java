163e724adb781ca5699a0acf2d2c61c74297665d
Owen Senowitz <owen.senowitz@gmail.com>
2022-09-07 22:28:32 -0400

    Two Sum completed

--- src/main/java/Two_Sum.java
+++ src/main/java/Two_Sum.java
@@ -1,2 +1,24 @@
+import java.util.Arrays;
+//https://leetcode.com/problems/two-sum/
 public class Two_Sum {
+    public static int[] twoSum(int[] nums, int target) {
+        int[] answerArray = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (i != j) {
+                    if (nums[i] + nums[j] == target) {
+                        answerArray[0] = i;
+                        answerArray[1] = j;
+                        return answerArray;
+                    }
+                }
+            }
+        }
+        return answerArray;
+    }
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+    }
 }
