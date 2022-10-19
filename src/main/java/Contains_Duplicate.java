8e7c107245c2df35dea7729ad5b5e4b1c230304f
Owen Senowitz <owen.senowitz@gmail.com>
2022-10-19 15:53:39 -0400

    Added links

--- src/main/java/Contains_Duplicate.java
+++ src/main/java/Contains_Duplicate.java
@@ -1,5 +1,6 @@
-import java.util.HashSet;
+//https://leetcode.com/problems/contains-duplicate/
 
+import java.util.HashSet;
 public class Contains_Duplicate {
     public static boolean containsDuplicate(int[] nums) {
         HashSet<Integer> set = new HashSet<>();
