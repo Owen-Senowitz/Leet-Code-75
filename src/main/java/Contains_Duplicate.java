28d2df9c594a867d812a1c4cbfe54befe7956eb8
Owen Senowitz <owen.senowitz@gmail.com>
2022-10-19 15:49:07 -0400

    Contains Duplicate 217 completed

---
+++ src/main/java/Contains_Duplicate.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+
+public class Contains_Duplicate {
+    public static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num: nums)
+            set.add(num);
+        return !(set.size() == nums.length);
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4};
+        containsDuplicate(nums);
+    }
+}
